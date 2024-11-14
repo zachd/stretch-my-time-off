67b49ee687449ef8bbd0cfe9f73bceea2209f115
Zachary <hello@zach.ie>
2024-11-14 14:17:42 +0100

    Switch to static site rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			assets: 'https://stretchmytimeoff.com', // Set your fully qualified domain here
+			assets: 'https://stretchmytimeoff.com',
 		}
 	}
 };
